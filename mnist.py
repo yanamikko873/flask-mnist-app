6aeccf74be5ed719d5a675d0af77fcf9e8edf666
yanamikko873 <GitHub>
2024-05-22 21:42:09 +0900

    コード更新

--- mnist.py
+++ mnist.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model = load_model('./model.h5')#学習済みモデルをロード
+model = load_model('./model.h5', compile=False)#学習済みモデルをロード
 
 
 @app.route('/', methods=['GET', 'POST'])
